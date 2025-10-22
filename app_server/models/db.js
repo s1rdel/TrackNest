172b2ca88e3d8b14cfaf21751a839b13e0168cc8
s1rdel <145557857+s1rdel@users.noreply.github.com>
2025-10-22 20:15:33 +0300

    Update db.js

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -3,7 +3,7 @@ mongoose.set('debug', true); // log queries
 mongoose.connection.once('open', () => {
   console.log('Connected DB name:', mongoose.connection.name);
 });
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://s1rdel:12345@cluster0.oc08hmu.mongodb.net/tracknest?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://<username>:<password>@cluster0.oc08hmu.mongodb.net/tracknest?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbURI, {})
    .then(() => console.log('Mongoose connected'))
